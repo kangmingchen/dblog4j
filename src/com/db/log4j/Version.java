46e350c269e364d2011769fb63f1a4dd9d9042fd
Chen KangMing <chenkm@ucweb.com>
2014-07-03 19:53:18 +0800

    修改版本号

--- src/com/db/log4j/Version.java
+++ src/com/db/log4j/Version.java
@@ -5,15 +5,18 @@ package com.db.log4j;
  * <p>Title:Version</p>
  * <p>Description:</p>
  * @Author Chenkangming @Date 2014-3-18
- * @version 2.0.5
+ * @version 2.0.6
  */
 public class Version {
 	/**
+	 * 2.0.6
+	 * 1、天数如果设置为-1则不执行删除日志
+	 * 
 	 * 2.0.5 改进功能
 	 * 1、返回站点更目录
 	 * 
 	 * 2.0.4 改进功能
 	 * <li>1、支持tomcat8</li>
 	 */
-	public static final String VERSION_INFO="DBLOG4J-2.0.5";
+	public static final String VERSION_INFO="DBLOG4J-2.0.6";
 }
